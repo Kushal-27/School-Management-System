8041f55bebe2059c067c926d35b71f83408067b5
kushal27 <kushalpoudel2058@gmail.com>
2025-03-20 03:34:43 +0545

    fix: user table name changed

--- school-management/src/main/java/com/school/management/model/User.java
+++ school-management/src/main/java/com/school/management/model/User.java
@@ -1,10 +1,10 @@
 package com.school.management.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+
 
 @Entity
-@Data
+@Table(name = "users")
 public class User {
 
     @Id
@@ -26,5 +26,65 @@ public class User {
 
     public enum Role {
         ADMIN, TEACHER, STUDENT
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, Role role, Student student, Teacher teacher) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.student = student;
+        this.teacher = teacher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
     }
 }
